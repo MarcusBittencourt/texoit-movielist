3ed90fcde409b0a5347f40cca37bff9508de9a2c
MarcusBittencourt <marcus.bittencourt@kamereon.dev>
2022-07-30 07:16:00 -0300

    Application initial setup

---
+++ movielist-api/src/test/java/br/com/texoit/movielist/movielistapi/MovielistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.texoit.movielist.movielistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
